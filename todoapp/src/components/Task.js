5177627a0ac1226903f53e35562ef4fe92c4d75d
Shanna <ssit002@fiu.edu>
2021-01-28 20:01:41 -0500

    Setup edit functionality

--- todoapp/src/components/Task.js
+++ todoapp/src/components/Task.js
@@ -12,9 +12,15 @@ import React, { useState } from "react";
 
 // THE COMPONENT =====================================================
 export default function Task(props) {
+  if (props.isEdit) {
+    return (
+      <li class="list-group-item">
+        <input type="text" value={`${props.task.id} + ${props.task.desc}`}/>
+        <button>delete</button>
+      </li>
+    )
+  }
   return (
-    <div class="container">
-      {props.task.id} + {props.task.desc}
-    </div>
+    <li class="list-group-item">{props.task.id} + {props.task.desc}</li>
   )
 }
--- todoapp/src/views/TaskView.js
+++ todoapp/src/views/TaskView.js
@@ -4,13 +4,48 @@ import Task from "../components/Task";
 
 // THE COMPONENT =====================================================
 export default function TaskView(props) {
+  const [isEditing, setIsEditing] = useState(false);
 
-  return (
-    <div class="container-fluid">
-      {props.list.tasks.map(task => {
-        return <Task key={task.id} task={task}/>
-      })}
-      <button type="button" className="btn btn-primary" onClick={() => props.changeView(0)}>Return</button>
-    </div>
-  )
+  const turnOnEditMode = () => {
+    setIsEditing(!isEditing);
+  }
+
+  if (isEditing) {
+    return (
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-6">
+            <input type="text" value={props.list.name} />
+            <ul className="list-group">
+              {props.list.tasks.map(task => {
+                return <Task key={task.id} task={task} isEdit={true} />
+              })}
+            </ul>
+          </div>
+        </div>
+        <button type="button" className="btn btn-primary" onClick={turnOnEditMode}>Return</button>
+        <button type="button" className="btn btn-primary">+</button>
+      </div>
+    )
+  }
+
+  else {
+    return (
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-6">
+            <h1>{props.list.name}</h1>
+            <ul className="list-group">
+              {props.list.tasks.map(task => {
+                return <Task key={task.id} task={task} isEdit={false} />
+              })}
+            </ul>
+          </div>
+        </div>
+        <button type="button" className="btn btn-primary" onClick={() => props.changeView(0)}>Return</button>
+        <button type="button" className="btn btn-danger" onClick={() => props.changeView(0)}>Delete</button>
+        <button type="button" className="btn btn-primary" onClick={turnOnEditMode}>Edit</button>
+      </div>
+    )
+  }
 }
